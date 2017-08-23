82165d6923e98992ada862c737f249b3fae9c5b4
cuishirui <344494024@qq.com>
2017-08-23 09:00:42 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!
   before_action :find_group_and_check_permission, only: [:update, :edit, :destroy]
 
   def index
@@ -42,6 +42,30 @@ class GroupsController < ApplicationController
     @post = Post.new
 
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 9)
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "joined this group"
+    else
+      flash[:warning] = "You alread is groups member"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:danger] = "quit this group"
+    else
+      flashp[:warning] = "You are not this group member, can't quit"
+    end
+
+    redirect_to group_path(@group)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'groups#index'
 
   resources :groups do
+    member do
+      post :join
+      post :quit 
+    end
     resources :posts
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
